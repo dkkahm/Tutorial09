37de994685c3cafe4e52a8848feacc8ecf4fde71
Kam Dongki <dkkahm@gmail.com>
2017-11-26 21:55:47 +0900

    p.191

---
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour {
+	
+	Transform player;
+	NavMeshAgent nav;
+
+	void Awake () {
+        Debug.Log("EnemyMove.Awake()");
+		
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <NavMeshAgent> ();
+
+	}
+	
+	void Update () {
+		if(nav.enabled){
+			nav.SetDestination (player.position);
+		}
+	}
+
+}
